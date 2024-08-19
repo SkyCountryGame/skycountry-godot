b24c9e5af17972b34a6e348c484f507056a6ef93
zach95564 <56219333+zach95564@users.noreply.github.com>
2024-08-18 21:58:21 -0400

    Added ArtScene + environment textures
    -Created new level called "ArtScene"
    -Imported dirt, grass-flowers, and grass textures from unity era
    -added objects to ArtScene

--- levels/AtmosphereManager.cs
+++ levels/AtmosphereManager.cs
@@ -1,18 +1,18 @@
 using Godot;
 
 public class AtmosphereManager {
-    private AudioStreamPlayer3D audPlayer;
+	private AudioStreamPlayer3D audPlayer;
 
-    public AtmosphereManager(){
-        audPlayer = new AudioStreamPlayer3D();
-        //http stream 
-        //HTTPStream httpStream = new HTTPStream();
-        //httpStream.SetDownloadLocation("http://example.com/audio.ogg"); // Replace with your audio URL
+	public AtmosphereManager(){
+		audPlayer = new AudioStreamPlayer3D();
+		//http stream 
+		//HTTPStream httpStream = new HTTPStream();
+		//httpStream.SetDownloadLocation("http://example.com/audio.ogg"); // Replace with your audio URL
 
-        //AudioStreamOGGVorbis oggVorbisStream = new AudioStreamOGGVorbis();
-        //oggVorbisStream.Stream = httpStream;
+		//AudioStreamOGGVorbis oggVorbisStream = new AudioStreamOGGVorbis();
+		//oggVorbisStream.Stream = httpStream;
 
-        //audPlayer.Stream = oggVorbisStream;
-        //audPlayer.Play();
-    }
+		//audPlayer.Stream = oggVorbisStream;
+		//audPlayer.Play();
+	}
 }
