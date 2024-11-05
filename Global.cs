e4138355620965525d42f87cc7c3175fae01c7f7
grothedev <grothedev@gmail.com>
2024-11-05 16:06:50 -0600

    restore recurse up scenetree in GetInteractable() that i erronously removed a while ago

--- Global.cs
+++ Global.cs
@@ -96,10 +96,18 @@ public partial class Global : Node
 		return null;
 	}
 
-	public static Interactable GetInteractable(Node n){
-		GameObject go = GetGameObject(n);
-		if (go != null && mapGameObjectToInteractable.ContainsKey(go)){
-			return mapGameObjectToInteractable[go];
+	//check if the given node is an interactable or a child of an interactable. set arg strict=true to only check single node
+	public static Interactable GetInteractable(Node n, bool strict = false){
+		if (strict){
+			if (n is Interactable interactable){ return interactable; }
+			else { return null; }
+		}
+		while (n != sceneTree.Root){
+			if (n is Interactable interactable)
+			{
+				return interactable;
+			}
+			n = n.GetParent();
 		}
 		return null;
 	}
