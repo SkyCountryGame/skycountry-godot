7fb655109f99f66ec8917af8ca15f75e8014aae8
mmstanke <79998511+mmstanke@users.noreply.github.com>
2024-11-20 21:04:10 -0600

    added new scenes
    added new scene connections for forest and test room

--- gameobjects/Rock.cs
+++ gameobjects/Rock.cs
@@ -3,52 +3,52 @@ using System;
 using System.ComponentModel;
 
 public partial class Rock : RigidBody3D, Interactable {
-    public InteractionType interactionType => InteractionType.Pickup;
+	public InteractionType interactionType => InteractionType.Pickup;
 
-    public InteractionMethod interactionMethod => InteractionMethod.Use;
+	public InteractionMethod interactionMethod => InteractionMethod.Use;
 
-    private InventoryItem rockItem;
+	private InventoryItem rockItem;
 
-    public Rock()
-    {
-    }
-
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
+	public Rock()
 	{
-        Global.RegisterGameObject(this, Name, GameObjectType.Interactable);
-        rockItem = new InventoryItem(InventoryItemProperties.ItemType.Mineral, "rock", false);
-        //GetChild<MeshInstance3D>(0).SetSurfaceMaterial(0, new SpatialMaterial() { AlbedoColor = new Color(0.5f, 0.5f, 0.5f) });
-    }
+	}
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		Global.RegisterGameObject(this, Name, GameObjectType.Interactable);
+		rockItem = new InventoryItem(InventoryItemProperties.ItemType.Mineral, "rock", false);
+		//GetChild<MeshInstance3D>(0).SetSurfaceMaterial(0, new SpatialMaterial() { AlbedoColor = new Color(0.5f, 0.5f, 0.5f) });
+	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 	}
 
-    public string Info()
-    {
-        return "Rock";
-    }
+	public string Info()
+	{
+		return "Rock";
+	}
 
-    //PAYLOAD 
-    public dynamic Interact()
-    {
-        return rockItem;
-    }
+	//PAYLOAD 
+	public dynamic Interact()
+	{
+		return rockItem;
+	}
 
-    public void Retain()
-    {
-        throw new NotImplementedException();
-    }
+	public void Retain()
+	{
+		throw new NotImplementedException();
+	}
 
-    public void Clear()
-    {
-        throw new NotImplementedException();
-    }
+	public void Clear()
+	{
+		throw new NotImplementedException();
+	}
 
-    public bool IsInteractionValid(Interactor interactor)
-    {
-        throw new NotImplementedException();
-    }
+	public bool IsInteractionValid(Interactor interactor)
+	{
+		throw new NotImplementedException();
+	}
 }
